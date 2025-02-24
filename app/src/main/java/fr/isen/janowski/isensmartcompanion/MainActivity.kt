27f534b96932042f626f92b6f5a1df2247cb03d4
ejanowski <janowski.emeric@gmail.com>
2025-02-24 11:45:07 +0100

    textfield + button

--- app/src/main/java/fr/isen/janowski/isensmartcompanion/MainActivity.kt
+++ app/src/main/java/fr/isen/janowski/isensmartcompanion/MainActivity.kt
@@ -1,16 +1,34 @@
 package fr.isen.janowski.isensmartcompanion
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.ImageButton
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import fr.isen.janowski.isensmartcompanion.ui.theme.ISENSmartCompanionTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,7 +39,7 @@ class MainActivity : ComponentActivity() {
             ISENSmartCompanionTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = getString(R.string.app_name),
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -32,10 +50,28 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    val context = LocalContext.current
+    var text = ""
+    Column(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally) {
+        Image(painterResource(R.drawable.isen),
+            context.getString(R.string.isen_logo))
+        Text(
+            text = "$name!",
+            modifier = modifier
+        )
+        Row {
+            TextField(text, {})
+            Button({
+                Log.d("testing", "button clicked")
+            },
+                content = {
+                Image(painterResource(R.drawable.send), "")
+            })
+        }
+
+    }
 }
 
 @Preview(showBackground = true)
