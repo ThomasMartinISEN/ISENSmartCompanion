86b9a576632c9ae3bbae6b9f82d29e3a6ca45019
ejanowski <janowski.emeric@gmail.com>
2025-02-24 15:17:26 +0100

    Part 1.3

--- app/src/main/java/fr/isen/janowski/isensmartcompanion/MainActivity.kt
+++ app/src/main/java/fr/isen/janowski/isensmartcompanion/MainActivity.kt
@@ -3,27 +3,41 @@ package fr.isen.janowski.isensmartcompanion
 import android.os.Bundle
 import android.util.Log
 import android.widget.ImageButton
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.foundation.layout.systemBars
+import androidx.compose.foundation.layout.windowInsetsTopHeight
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
@@ -38,10 +52,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             ISENSmartCompanionTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = getString(R.string.app_name),
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Box(Modifier.padding(innerPadding)) {
+                        MainScreen(innerPadding)
+                    }
                 }
             }
         }
@@ -49,23 +62,39 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun MainScreen(innerPadding: PaddingValues) {
     val context = LocalContext.current
-    var text = ""
+    var userInput = remember { mutableStateOf("") }
     Column(
-        modifier = Modifier.fillMaxWidth(),
+        modifier = Modifier.fillMaxWidth()
+            .fillMaxSize()
+            .padding(innerPadding),
         horizontalAlignment = Alignment.CenterHorizontally) {
         Image(painterResource(R.drawable.isen),
             context.getString(R.string.isen_logo))
-        Text(
-            text = "$name!",
-            modifier = modifier
-        )
-        Row {
-            TextField(text, {})
-            Button({
-                Log.d("testing", "button clicked")
-            },
+        Text(context.getString(R.string.app_name))
+        Text("", modifier = Modifier
+            .fillMaxSize()
+            .weight(0.5F))
+        Row(modifier = Modifier
+            .fillMaxWidth()
+            .clip(RoundedCornerShape(16.dp))
+            .background(Color.LightGray)
+            .padding(8.dp)) {
+            TextField(
+                value = userInput.value,
+                onValueChange = { newValue ->
+                    userInput.value = newValue
+                },
+                colors = TextFieldDefaults.colors(
+                    focusedContainerColor = Color.Transparent,
+                    unfocusedContainerColor = Color.Transparent,
+                    disabledContainerColor = Color.Transparent,
+                    errorContainerColor = Color.Transparent),
+                modifier = Modifier.weight(1F))
+            OutlinedButton ( onClick = {
+                Toast.makeText(context, "user input : ${userInput.value}", Toast.LENGTH_LONG).show()
+            },  modifier = Modifier.background(Color.Red, shape = RoundedCornerShape(50)),
                 content = {
                 Image(painterResource(R.drawable.send), "")
             })
@@ -78,6 +107,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     ISENSmartCompanionTheme {
-        Greeting("Android")
+        MainScreen(PaddingValues(8.dp))
     }
 }
